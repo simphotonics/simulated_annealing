d9875af35ee1b1695591eb3246f9746f2cfda419
simphotonics <git@simphotonics.com>
2021-02-03 22:55:59 +0000

    Moved class LoggingSimulator to lib folder.

--- example/bin/simulated_annealing_example.dart
+++ example/bin/simulated_annealing_example.dart
@@ -1,106 +1,21 @@
 import 'dart:io';
-import 'dart:math';
 
 import 'package:simulated_annealing/simulated_annealing.dart';
 
-class LoggingSimulator extends Simulator {
-  LoggingSimulator(
-    Energy system,
-    AnnealingSchedule schedule, {
-    num gamma = 0.8,
-    num? dE0,
-    List<num>? xMin0,
-  }) : super(
-          system,
-          schedule,
-          gamma: gamma,
-          dE0: dE0,
-          xMin0: xMin0,
-        );
-
-  final rec = DataRecorder();
-
-  @override
-  void prepareLog() {
-    rec.prepareVector('x', 3);
-    rec.prepareScalar('Energy');
-    rec.prepareScalar('Energy Min');
-    rec.prepareScalar('P(dE)');
-    rec.prepareScalar('Temperature');
-    rec.prepareVector('dx', 3);
-  }
-
-  @override
-  void recordLog() {
-    rec.addVector('x', x);
-    rec.addVector('dx', dx);
-    rec.addScalar('Energy', eCurrent);
-    rec.addScalar('Energy Min', eMin);
-    rec.addScalar('P(dE)',
-        (eCurrent - eMin) < 0 ? 1 : exp(-(eCurrent - eMin) / (kB * t)));
-    rec.addScalar('Temperature', t);
-  }
-}
-
+import 'energy_field_example.dart';
+/// To run this program navigate to the folder `example/bin` in your local
+/// copy of the package `simulated_annealing` and use the command:
+/// $ dart simulated_annealing_example.dart
 void main() async {
-  // Defining a spherical space.
-  final radius = 2;
-  final x = FixedInterval(-radius, radius);
-  final y = ParametricInterval(
-    () => -sqrt(pow(radius, 2) - pow(x.next(), 2)),
-    () => sqrt(pow(radius, 2) - pow(x.next(), 2)),
-  );
-  final z = ParametricInterval(
-    () => -sqrt(pow(radius, 2) - pow(y.next(), 2) - pow(x.next(), 2)),
-    () => sqrt(pow(radius, 2) - pow(y.next(), 2) - pow(x.next(), 2)),
-  );
-  final space = SearchSpace([x, y, z]);
-
-  // Defining an annealing schedule.
-  final schedule = AnnealingSchedule(
-    exponentialSequence(100, 1e-8, n: 750),
-    space.size,
-    [1e-6, 1e-6, 1e-6],
-  );
-
-  // Defining an energy function.
-  // The energy function has a local minimum at xLocalMin
-  // and a global minimum at xGlobalMin.
-  final xGlobalMin = [0.5, 0.7, 0.8];
-  final xLocalMin = [-1.0, -1.0, -0.5];
-  num energyFunction(List<num> x) {
-    return 4.0 -
-        4.0 * exp(-4 * xGlobalMin.distance(x)) -
-        2.0 * exp(-6 * xLocalMin.distance(x));
-  }
-
-  // ignore: unused_element
-  int markov(num temperature) {
-    return min(1 + 1 ~/ (100 * temperature), 25);
-  }
-
-  final energy = Energy(energyFunction, space);
-
   // Construct a simulator instance.
-  final simulator = LoggingSimulator(
-    energy,
-    schedule,
-    gamma: 0.8,
-    dE0: energy.stdDev + 0.1,
-    xMin0: [-1, -1, -0.5],
-  );
+  final simulator = LoggingSimulator(energyField, exponentialSequence,
+      perturbationSequence,
+      iterations: 750, gammaStart: 0.7, gammaEnd: 0.05);
 
-  print(simulator);
+  print(await simulator.info);
 
-  final sample = simulator.system.samplePoints;
-  for (var i = 0; i < simulator.system.sampleSize; i++) {
-    sample[i].add(simulator.system.sample[i]);
-  }
-
-  final xSol = simulator.anneal((t) => 1);
+  final xSol = await simulator.anneal((_) => 1, isRecursive: true);
   await File('../data/log.dat').writeAsString(simulator.rec.export());
-  await File('../data/energy_sample.dat')
-      .writeAsString(sample.export(label: 'x y z energy'));
 
   print('Solution: $xSol');
 }
