b891c1d88c7825ea94518f6377180c30bbc05142
simphotonics <git@simphotonics.com>
2021-02-03 22:44:19 +0000

    Added export statements.

--- lib/simulated_annealing.dart
+++ lib/simulated_annealing.dart
@@ -2,10 +2,10 @@
 library simulated_annealing;
 
 export 'src/framework/annealing_schedule.dart';
-export 'src/framework/energy.dart';
+export 'src/framework/energy_field.dart';
+export 'src/framework/energy_statistics.dart';
 export 'src/framework/data_recorder.dart';
+export 'src/framework/logging_simulator.dart';
 export 'src/framework/search_space.dart';
 export 'src/framework/simulator.dart';
-export 'src/extensions/euclidean_metric.dart';
-export 'src/extensions/list_utils.dart';
 export 'src/extensions/random_sample.dart';
