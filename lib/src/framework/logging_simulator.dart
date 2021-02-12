675424df9e8cb4f8f0617f4b358631c35edde78e
simphotonics <git@simphotonics.com>
2021-02-12 19:03:29 +0000

    Made data recorder private.

--- lib/src/framework/logging_simulator.dart
+++ lib/src/framework/logging_simulator.dart
@@ -57,18 +57,71 @@ class LoggingSimulator extends Simulator {
           dEnergyEnd: dEnergyEnd,
         );
 
-  final rec = NumericalDataRecorder();
+  /// Records the simulator log.
+  final _rec = NumericalDataRecorder();
+
+  String export({
+    int precision = 10,
+    String delimiter = '   ',
+  }) =>
+      _rec.export(
+        precision: precision,
+        delimiter: delimiter,
+      );
+
+  String exportLast({
+    int precision = 10,
+    String delimiter = '   ',
+  }) =>
+      _rec.exportLast(
+        precision: precision,
+        delimiter: delimiter,
+      );
+
+  String exportFirst({
+    int precision = 10,
+    String delimiter = '   ',
+  }) =>
+      _rec.exportLast(
+        precision: precision,
+        delimiter: delimiter,
+      );
+
+  /// Returns the current position log.
+  List<List<num>> get currentPositionLog => _rec.getVector('x');
+
+  /// Returns the current perturbation magnitude log.
+  List<List<num>> get dxLog => _rec.getVector('dx');
+
+  /// Returns the current energy log.
+  List<num> get currentEnergyLog => _rec.getScalar('Energy');
+
+  /// Returns the current min. energy log.
+  List<num> get currentMinEnergyLog => _rec.getScalar('Energy Min');
+
+  /// Returns the current acceptance probability log.
+  List<num> get acceptanceProbabilityLog => _rec.getScalar('P(dE > 0)');
+
+  /// Returns the current temperature log.
+  List<num> get temperatureLog => _rec.getScalar('Temperature');
+
+  /// The number of times `recordLog()` was called.
+  int _logCount = 0;
+
+  /// Returns the number times `recordLog()` was called.
+  int get logCount => _logCount;
 
   @override
   void prepareLog() {}
 
   @override
   void recordLog() {
-    rec.addVector('x', currentPosition);
-    rec.addVector('dx', dx);
-    rec.addScalar('Energy', currentEnergy);
-    rec.addScalar('Energy Min', currentMinEnergy);
-    rec.addScalar('P(dE > 0)', acceptanceProbability);
-    rec.addScalar('Temperature', t);
+    _rec.addVector('x', currentPosition);
+    _rec.addVector('dx', dx);
+    _rec.addScalar('Energy', currentEnergy);
+    _rec.addScalar('Energy Min', currentMinEnergy);
+    _rec.addScalar('P(dE > 0)', acceptanceProbability);
+    _rec.addScalar('Temperature', t);
+    ++_logCount;
   }
 }
