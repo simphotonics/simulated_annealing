97b2c947b6caa40cab2918854d3992419cec0ddb
simphotonics <git@simphotonics.com>
2021-02-20 14:24:22 +0000

    Amended simulator params.

--- example/bin/simulated_annealing_example.dart
+++ example/bin/simulated_annealing_example.dart
@@ -3,18 +3,24 @@ import 'dart:io';
 import 'package:simulated_annealing/simulated_annealing.dart';
 
 import 'energy_field_example.dart';
+
 /// To run this program navigate to the folder `example/bin` in your local
 /// copy of the package `simulated_annealing` and use the command:
 /// $ dart simulated_annealing_example.dart
 void main() async {
   // Construct a simulator instance.
-  final simulator = LoggingSimulator(field, exponentialSequence,
-      perturbationSequence,
-      iterations: 750, gammaStart: 0.7, gammaEnd: 0.05);
+  final simulator = LoggingSimulator(
+    field,
+    exponentialSequence,
+    perturbationSequence,
+    iterations: 450,
+    gammaStart: 0.7,
+    gammaEnd: 0.05,
+  );
 
   print(await simulator.info);
 
-  final xSol = await simulator.anneal((_) => 1, isRecursive: true);
+  final xSol = await simulator.anneal((_) => 3, isRecursive: true, ratio: 0.5);
   await File('../data/log.dat').writeAsString(simulator.export());
 
   print('Solution: $xSol');
