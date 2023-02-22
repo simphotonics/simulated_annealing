e325fabc6fcd8d3b00c87675e09c2c8f5e051090
simphotonics <git@simphotonics.com>
2023-02-22 17:08:24 +0000

    Replaced variable names.

--- lib/src/extensions/random_in_range.dart
+++ lib/src/extensions/random_in_range.dart
@@ -3,64 +3,64 @@ import 'dart:math';
 import 'package:exception_templates/exception_templates.dart';
 
 /// Inverse cummulative distribution function of a probability distribution
-/// function with non-zero support over the interval: `[xMin, xMax)`.
-/// The function must return a numerical value in the interval: `[xMin, xMax)`.
+/// function with non-zero support over the interval: `[start, end)`.
+/// The function must return a numerical value in the interval: `[start, end)`.
 /// * `p`: A probability `0 <= p < 1`,
-/// * `xMin`: the lower limit,
-/// * `xMax`: the upper limit.
-typedef InverseCdf = num Function(num p, num xMin, num xMax);
+/// * `start`: the lower limit,
+/// * `end`: the upper limit.
+typedef InverseCdf = num Function(num p, num start, num end);
 
 /// Extension on [Random] providing the methods
 /// `nextInRange` and `nextIntFromList`.
 extension RandomInRange on Random {
   /// Generates a random floating point
-  /// value in the range from `xMin`,
-  /// inclusive, to `xMax`, exclusive.
-  /// * `xMin`: left boundary of the interval.
-  /// * `xMax`: right boundary of the interval, `xMin <= xMax`.
+  /// value in the range from `start`,
+  /// inclusive, to `end`, exclusive.
+  /// * `start`: left boundary of the interval.
+  /// * `end`: right boundary of the interval, `start <= end`.
   /// * `inverseCdf`: inverse of the cummulative
   ///   probability distribution function  with
-  ///   non-zero support over the range [xMin,&nbsp;xMax).
+  ///   non-zero support over the range [start,&nbsp;end).
   /// * `nGrid`: Number of grid points.
   ///   If `nGrid > 1` the interval
   ///   is divided into an equidistant grid
-  ///   with `nGrid` points: `[xMin + dx, xMin + 2 * dx, ..., xMax - dx]`
-  ///   where `dx = (xMax - xMin) / nGrid` and any random number returned
-  ///   coincides with a gridpoint `xMin` exclusive, `xMax` exclusive.
+  ///   with `nGrid` points: `[start + dx, start + 2 * dx, ..., end - dx]`
+  ///   where `dx = (end - start) / nGrid` and any random number returned
+  ///   coincides with a gridpoint `start` exclusive, `end` exclusive.
   /// ---
   /// * If `inverseCdf == null` it is assumed that the values are
   ///   uniformly distributed.
   /// * If `inverseCdf` is non-null, the random value is generated using:
-  ///   `inverseCdf(nextDouble(), xMin, xMax)`.
-  /// * Note: The function `inverseCdf(p, xMin, xMax)` must return a
-  ///   value in the range `[xMin, xMax)` for each argument `p`
+  ///   `inverseCdf(nextDouble(), start, end)`.
+  /// * Note: The function `inverseCdf(p, start, end)` must return a
+  ///   value in the range `[start, end)` for each argument `p`
   ///   in the range `[0,1)`.
   num nextInRange(
-    num xMin,
-    num xMax, {
+    num start,
+    num end, {
     InverseCdf? inverseCdf,
     int nGrid = 0,
   }) {
-    if (xMin == xMax) {
-      return xMax;
+    if (start == end) {
+      return end;
     }
     if (nGrid < 2) {
       return inverseCdf == null
-          ? xMin + nextDouble() * (xMax - xMin)
-          : inverseCdf(nextDouble(), xMin, xMax);
+          ? start + nextDouble() * (end - start)
+          : inverseCdf(nextDouble(), start, end);
     } else {
-      final dx = (xMax - xMin) / (nGrid);
+      final dx = (end - start) / (nGrid);
       if (inverseCdf == null) {
-        return xMin +
+        return start +
             0.5 * dx +
-            (((1.0 - 1.0 / nGrid) * nextDouble() * (xMax - xMin)) / dx)
+            (((1.0 - 1.0 / nGrid) * nextDouble() * (end - start)) / dx)
                     .roundToDouble() *
                 dx;
       } else {
-        return (xMin +
+        return (start +
             0.5 * dx +
-            (((1.0 - 1.0 / nGrid) * inverseCdf(nextDouble(), xMin, xMax) -
-                            xMin) /
+            (((1.0 - 1.0 / nGrid) * inverseCdf(nextDouble(), start, end) -
+                            start) /
                         dx)
                     .roundToDouble() *
                 dx);
@@ -70,15 +70,15 @@ extension RandomInRange on Random {
 
   /// Returns a list containing the grid points of an interval
   /// with
-  /// * `xMin`: left boundary,
-  /// * `xMax`: right boundary,
+  /// * `start`: left boundary,
+  /// * `end`: right boundary,
   /// * `nGrid`: number of grid points.
   ///
-  /// Returns an empty list if `grid < 2`.
-  List<num> gridPoints(num xMin, num xMax, int nGrid) {
+  /// Returns an empty list if `nGrid < 2`.
+  List<num> gridPoints(num start, num end, int nGrid) {
     if (nGrid < 2) return [];
-    final dx = (xMax - xMin) / (nGrid);
-    final x0 = xMin + 0.5 * dx;
+    final dx = (end - start) / (nGrid);
+    final x0 = start + 0.5 * dx;
     final result = List<num>.generate(nGrid, (i) => x0 + i * dx);
     return result;
   }
