c91f188c2d219c2e6023f9cd477c90e253418f6c
simphotonics <git@simphotonics.com>
2023-03-13 21:34:05 +0000

    Removed library title.

--- lib/simulated_annealing.dart
+++ lib/simulated_annealing.dart
@@ -1,11 +1,10 @@
-/// Simulated annealing framework for Dart.
-library simulated_annealing;
+/// Simulated Annealing framework for Dart.
 
+export 'src/extensions/random_in_range.dart';
 export 'src/framework/annealing_schedule.dart';
-export 'src/framework/energy_field.dart';
 export 'src/framework/data_recorder.dart';
+export 'src/framework/energy_field.dart';
 export 'src/framework/interval.dart';
 export 'src/framework/logging_simulator.dart';
 export 'src/framework/search_space.dart';
 export 'src/framework/simulator.dart';
-export 'src/extensions/random_in_range.dart';
